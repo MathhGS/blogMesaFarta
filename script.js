8e930ae2d954ce224d6631d3f397db6ca5712720
Matheus Gonçalves <matheusgon503@gmail.com>
2025-11-16 21:24:27 -0300

    Fix: GitHub Pages

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     
-    fetch("components/_header.html") 
+    fetch("_header.html") 
         .then(response => response.text()) 
         .then(data => {
             const headerPlaceholder = document.getElementById("header-placeholder")
             if (headerPlaceholder) headerPlaceholder.innerHTML = data;
         })
 
-    fetch("components/_footer.html") 
+    fetch("_footer.html") 
         .then(response => response.text()) 
         .then(data => {
             const footerPlaceholder = document.getElementById("footer-placeholder")
